e3305ac4685f03553ce04968dc1b7aa1aacd24f0
Leandro Biloni <leandrobiloni@hotmail.com>
2022-11-28 00:53:12 -0300

    Add Asset Renamer

---
+++ Editor/AssetRenamer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Utilities.AssetDatabaseUtils
+{
+    public static class AssetRenamer
+    {
+        public static void RenameAsset(Object obj, string newName)
+        {
+            string path = AssetDatabase.GetAssetPath(obj);
+
+            AssetDatabase.RenameAsset(path, newName);
+            AssetDatabase.Refresh();
+        }
+    }
+}
